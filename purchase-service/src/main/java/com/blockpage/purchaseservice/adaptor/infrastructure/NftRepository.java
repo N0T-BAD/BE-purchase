200f778df7b2b7f055fb0d3c95a1764668354b55
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-03 16:56:38 +0900

    feat: NFT JPA 레포지토리 생성

---
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/NftRepository.java
@@ -0,0 +1,7 @@
+package com.blockpage.purchaseservice.adaptor.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NftRepository extends JpaRepository<NftEntity, Long> {
+
+}
