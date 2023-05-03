81a84589c816425802905013e9285bb5c1eb8f49
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-03 16:56:58 +0900

    feat: NFT 타입 관리용 파일 생성

---
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/NftType.java
@@ -0,0 +1,24 @@
+package com.blockpage.purchaseservice.adaptor.infrastructure;
+
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum NftType {
+
+    ONE_BLOCK_DISCOUNT_BY_AUTHOR(0, "작가별 블럭 1개 할인권"),
+    TWO_BLOCK_DISCOUNT_BY_AUTHOR(1, "작가별 블럭 2개 할인권"),
+    ;
+
+    private int key;
+    private String value;
+
+    public static NftType findNftTypeByKey(int num) {
+        return Arrays.stream(NftType.values())
+            .filter(t -> t.getKey() == num)
+            .findFirst()
+            .get();
+    }
+}
