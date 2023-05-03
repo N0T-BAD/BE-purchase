225923c2810c4f848b60de620bbc8f1721c26f92
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-03 16:56:19 +0900

    feat: NFT 엔티티 생성

---
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/NftEntity.java
@@ -0,0 +1,46 @@
+package com.blockpage.purchaseservice.adaptor.infrastructure;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "nft")
+public class NftEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long userId;
+
+    @Column
+    private Long creatorId;
+
+    @Column
+    private String nftName;
+
+    @Column
+    private String nftDescription;
+
+    @Column
+    private Integer nftBlockPrice;
+
+    @Column
+    private Integer nftStock;
+
+    @Column
+    private String nftImage;
+
+    @Enumerated(EnumType.STRING)
+    private NftType nftType;
+
+}
