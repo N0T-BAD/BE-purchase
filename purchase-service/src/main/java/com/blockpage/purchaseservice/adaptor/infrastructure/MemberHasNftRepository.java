71ca97864e2c81eee1f9f400a2de162ceee5c4a9
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-04 10:39:56 +0900

    fix: NFT 관련 JPA 레포지토리 수정

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/MemberHasNftRepository.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/MemberHasNftRepository.java
@@ -3,7 +3,7 @@ package com.blockpage.purchaseservice.adaptor.infrastructure;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NftRepository extends JpaRepository<NftEntity, Long> {
+public interface MemberHasNftRepository extends JpaRepository<MemberHasNftEntity,Long> {
 
-    List<NftEntity> findByUserId(Long userId);
+    List<MemberHasNftEntity> findByMemberId(Long memberId);
 }
