dd98037a867cfebdfc993f5cb096483799a632f0
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-04 10:42:19 +0900

    feat: 웹툰 구매 타입 무료 타입 추가

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/PersistType.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/PersistType.java
@@ -7,8 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PersistType {
-    PERMANENT(0, "영구 소장"),
-    RENTAL(1, "대여"),
+    FREE(0, "무료"),
+    PERMANENT(1, "영구 소장"),
+    RENTAL(2, "대여"),
     ;
     private int key;
     private String value;
