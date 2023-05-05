c9b0018ebca7e708cbd592b2cec82d7f2834a8e7
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-05 19:00:50 +0900

    fix: api spec List 형식은 mock 데이터를 복수형으로 변경

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/web/controller/ProductController.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/web/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.blockpage.purchaseservice.adaptor.infrastructure.value.NftType;
 import com.blockpage.purchaseservice.adaptor.infrastructure.value.WebtoonBMStatus;
 import com.blockpage.purchaseservice.adaptor.web.apispec.ApiWrapperResponse;
 import com.blockpage.purchaseservice.adaptor.web.view.ProductsDetailView;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,26 +28,38 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<ApiWrapperResponse> getAllProducts(@RequestParam String type) {
 
-        ProductsDetailView productsDetailView = null;
+        List<ProductsDetailView> productsDetailViews = new ArrayList<>();
 
         switch (type) {
             case "nft": {
-                productsDetailView = new ProductsDetailView(1L, 1L, "NFT이름", "조석 작가님의 작품을 1BLOCK할인 받을 수 있습니다.", 1000,
+                productsDetailViews.add(new ProductsDetailView(1L, 1L, "NFT이름", "조석 작가님의 작품을 1BLOCK할인 받을 수 있습니다.", 1000,
                     "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp",
-                    NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR);
+                    NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR));
+                productsDetailViews.add(new ProductsDetailView(2L, 2L, "NFT이름", "조석 작가님의 작품을 1BLOCK할인 받을 수 있습니다.", 1000,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp",
+                    NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR));
+                productsDetailViews.add(new ProductsDetailView(3L, 3L, "NFT이름", "조석 작가님의 작품을 2BLOCK할인 받을 수 있습니다.", 2000,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp",
+                    NftType.TWO_BLOCK_DISCOUNT_BY_AUTHOR));
             }
             break;
             case "profile-skin": {
-                productsDetailView = new ProductsDetailView(1L, "플레티넘 테두리", "한달 동안 출석체크를 하면 받을 수 있습니다.", 200,
-                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp");
+                productsDetailViews.add(new ProductsDetailView(1L, "플레티넘 테두리", "한달 동안 출석체크를 하면 받을 수 있습니다.", 5000,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp"));
+                productsDetailViews.add(new ProductsDetailView(2L, "골드 테두리", "2주동안 출석체크를 하면 받을 수 있습니다.", 2000,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp"));
+                productsDetailViews.add(new ProductsDetailView(3L, "실버 테두리", "회원가입시 받을 수 있습니다.", 200,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp"));
 
             }
             break;
             case "episode-bm": {
-                productsDetailView = new ProductsDetailView(1L, 1L, 1L, 2, WebtoonBMStatus.ON_SALE);
+                productsDetailViews.add(new ProductsDetailView(1L, 1L, 1L, 2, WebtoonBMStatus.ON_SALE));
+                productsDetailViews.add(new ProductsDetailView(2L, 1L, 2L, 2, WebtoonBMStatus.ON_SALE));
+                productsDetailViews.add(new ProductsDetailView(3L, 1L, 3L, 2, WebtoonBMStatus.ON_SALE));
             }
         }
         return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiWrapperResponse(productsDetailView));
+            .body(new ApiWrapperResponse(productsDetailViews));
     }
 }
--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/web/controller/PurchaseController.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/web/controller/PurchaseController.java
@@ -7,6 +7,8 @@ import com.blockpage.purchaseservice.adaptor.web.apispec.ApiWrapperResponse;
 
 import com.blockpage.purchaseservice.adaptor.web.view.MemberPurchaseView;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
@@ -29,25 +31,39 @@ public class PurchaseController {
      */
     @GetMapping
     public ResponseEntity<ApiWrapperResponse> getMemberPurchaseHistory(@RequestParam("type") String type) {
-        MemberPurchaseView memberPurchaseView = null;
+
+        List<MemberPurchaseView> memberPurchaseViews = new ArrayList<>();
         switch (type) {
             case "nft": {
-                memberPurchaseView = new MemberPurchaseView(1L, "조석 작가 할인권", NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR,
-                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp");
+                memberPurchaseViews.add(new MemberPurchaseView(1L, "조석 작가 할인권", NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp"));
+                memberPurchaseViews.add(new MemberPurchaseView(2L, "박형석 작가 할인권", NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp"));
+                memberPurchaseViews.add(new MemberPurchaseView(3L, "정승훈 작가 할인권", NftType.ONE_BLOCK_DISCOUNT_BY_AUTHOR,
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp"));
             }
             break;
             case "profile-skin": {
-                memberPurchaseView = new MemberPurchaseView(1L, "플레티넘 테두리",
+                memberPurchaseViews.add(new MemberPurchaseView(1L, "플레티넘 테두리",
                     "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp",
-                    true);
+                    true));
+                memberPurchaseViews.add(new MemberPurchaseView(2L, "골드 테두리",
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp",
+                    false));
+                memberPurchaseViews.add(new MemberPurchaseView(3L, "실버 테두리",
+                    "https://i.namu.wiki/i/xWl9taYDaPvw2szKR9xbwBRYmVS8C7yvb4sn0jEKhaadR7OcKt_Voaz7gLEWlXq2Ji_s-T2WDjhMKs2uVSOM2drDi7tFTo0kkH7k-xwHrrKYT02zurwVXCYfVbyzePYOgdEpExfdPGS0PJFYEciiRA.webp",
+                    false));
             }
             break;
             case "episode-bm": {
-                memberPurchaseView = new MemberPurchaseView(1L, 1L, PersistType.PERMANENT, LocalDateTime.now());
+                memberPurchaseViews.add(new MemberPurchaseView(1L, 50L, PersistType.PERMANENT, LocalDateTime.now()));
+                memberPurchaseViews.add(new MemberPurchaseView(2L, 220L, PersistType.RENTAL, LocalDateTime.now()));
+                memberPurchaseViews.add(new MemberPurchaseView(3L, 133L, PersistType.RENTAL, LocalDateTime.now()));
+                memberPurchaseViews.add(new MemberPurchaseView(4L, 11L, PersistType.FREE, LocalDateTime.now()));
             }
             break;
         }
         return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiWrapperResponse(memberPurchaseView));
+            .body(new ApiWrapperResponse(memberPurchaseViews));
     }
 }
