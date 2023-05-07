e84cde353e2e04d7431eda95ae3c57b16b6f333b
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-08 00:09:31 +0900

    feat: 구매 도메인 port Adaptor 사이 연결 DTO mapping 함수 구현

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/entity/MemberHasEpisodeBMEntity.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/entity/MemberHasEpisodeBMEntity.java
@@ -1,6 +1,7 @@
 package com.blockpage.purchaseservice.adaptor.infrastructure.entity;
 
 import com.blockpage.purchaseservice.adaptor.infrastructure.value.PersistType;
+import com.blockpage.purchaseservice.application.port.out.PurchaseDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,8 +11,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 @Entity
 @Table(name = "member_has_episode_bm")
@@ -35,4 +42,14 @@ public class MemberHasEpisodeBMEntity {
 
     @Column
     private LocalDateTime expiredDate;
+
+    public static MemberHasEpisodeBMEntity toEntity(PurchaseDto purchaseDto) {
+        return MemberHasEpisodeBMEntity.builder()
+            .memberId(purchaseDto.getMemberId())
+            .episodeId(purchaseDto.getEpisodeId())
+            .webtoonId(purchaseDto.getWebtoonId())
+            .persistType(purchaseDto.getPersistType())
+            .expiredDate(purchaseDto.getExpiredDate())
+            .build();
+    }
 }
--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/entity/MemberHasProfileSkinEntity.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/entity/MemberHasProfileSkinEntity.java
@@ -1,5 +1,7 @@
 package com.blockpage.purchaseservice.adaptor.infrastructure.entity;
 
+import com.blockpage.purchaseservice.application.port.out.PurchaseDto;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,18 +11,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @Entity
 @Table(name = "member_has_profile_skin")
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberHasProfileSkinEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "profile_skin_id")
     private ProfileSkinEntity profileSkinEntity;
 
@@ -29,4 +37,12 @@ public class MemberHasProfileSkinEntity {
 
     @Column(name = "default_skin")
     private Boolean defaultSkin;
+
+    public static MemberHasProfileSkinEntity toEntity(PurchaseDto purchaseDto) {
+        return MemberHasProfileSkinEntity.builder()
+            .profileSkinEntity(new ProfileSkinEntity()) //상품 Entity 넣어줘야함..!
+            .memberId(purchaseDto.getMemberId())
+            .defaultSkin(Boolean.FALSE)
+            .build();
+    }
 }
