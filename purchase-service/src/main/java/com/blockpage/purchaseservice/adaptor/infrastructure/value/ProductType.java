f1ddd09bc07fb6b65f814d1789bf6fadfd1b8b68
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-08 11:30:51 +0900

    feat: 상품 도메인 구분 Enum 생성

---
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/value/ProductType.java
@@ -0,0 +1,23 @@
+package com.blockpage.purchaseservice.adaptor.infrastructure.value;
+
+import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ProductType {
+    EPISODE_BM(0, "episode-bm"),
+    PROFILE_SKIN(1, "profile-skin"),
+    NFT(2, "nft"),
+    ;
+    private int key;
+    private String value;
+
+    public static ProductType findPersistTypeByValue(String value) {
+        return Arrays.stream(ProductType.values())
+            .filter(t -> t.getValue().equals(value))
+            .findFirst()
+            .get();
+    }
+}
