0964c32508c34c37b89b4a55999c90d9dae7d3a4
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-09 22:53:09 +0900

    rename: 결제를 하는 도메인임으로 카카오페이를 특정하는 이름 대신 결제로 변경

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/web/controller/PaymentController.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/web/controller/PaymentController.java
@@ -17,25 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/payments")
-public class KakaoController {
-
-    private String KAKAO_PAY = "kakaopay";
+public class PaymentController {
 
     private final KakaoPayUseCase kakaoPayUseCase;
 
-    @PostMapping
-    public ResponseEntity<ApiWrapperResponse> postPayment(
-        @RequestParam String type,
-        @RequestBody KakaoPayReadyRequest kakaoPayReadyRequest) {
+    @PostMapping("/kakaopay/ready")
+    public ResponseEntity<ApiWrapperResponse> postKakaoPayReady(@RequestBody KakaoPayReadyRequest kakaoPayReadyRequest) {
         Long memberId = 1L;
-        if (type.equals(KAKAO_PAY)) {
-            KakaoPayReadyView kakaoPayReadyView = kakaoPayUseCase.kakaoPayReady(
-                KakaoPayInPortDto.toInPortDto(memberId, kakaoPayReadyRequest));
-            return ResponseEntity.status(HttpStatus.OK)
-                .body(new ApiWrapperResponse(kakaoPayReadyView));
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ApiWrapperResponse("잘못된 요청 입니다."));
-        }
+
+        KakaoPayReadyView kakaoPayReadyView = kakaoPayUseCase.kakaoPayReady(KakaoPayInPortDto.toInPortDto(memberId, kakaoPayReadyRequest));
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new ApiWrapperResponse(kakaoPayReadyView));
     }
 }
+
