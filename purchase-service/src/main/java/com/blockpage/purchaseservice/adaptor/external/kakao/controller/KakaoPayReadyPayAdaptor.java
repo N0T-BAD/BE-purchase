77f71d58af1855ea9b14f066eff982806398098f
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-10 00:26:00 +0900

    rename: port 이름 변경

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/external/kakao/controller/KakaoPayReadyPayAdaptor.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/external/kakao/controller/KakaoPayReadyPayAdaptor.java
@@ -2,13 +2,13 @@ package com.blockpage.purchaseservice.adaptor.external.kakao.controller;
 
 import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayReadyParams;
 import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayReadyResponse;
-import com.blockpage.purchaseservice.application.port.out.KakaoReadyPort;
+import com.blockpage.purchaseservice.application.port.out.KakaoPayReadyPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class KakaoReadyPayAdaptor implements KakaoReadyPort {
+public class KakaoPayReadyPayAdaptor implements KakaoPayReadyPort {
 
     private final KakaoPayFeignClient kakaoPayFeignClient;
 
--- purchase-service/src/main/java/com/blockpage/purchaseservice/application/port/out/KakaoPayReadyPort.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/application/port/out/KakaoPayReadyPort.java
@@ -3,7 +3,7 @@ package com.blockpage.purchaseservice.application.port.out;
 import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayReadyParams;
 import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayReadyResponse;
 
-public interface KakaoReadyPort {
+public interface KakaoPayReadyPort {
 
     KakaoPayReadyResponse ready(KakaoPayReadyParams kakaoPayReadyParams);
 }
