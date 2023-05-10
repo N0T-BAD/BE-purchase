ff01fc740982c993bd9df314aaed69110387219d
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-10 23:32:55 +0900

    fix: Redis 구성 설정 변경 utf-8 문자열 코드로 저장하도록 변경

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/redis/RedisConfig.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/redis/RedisConfig.java
@@ -1,14 +1,20 @@
 package com.blockpage.purchaseservice.adaptor.infrastructure.redis;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final RedisProperties redisProperties;
 
     @Value("${spring.redis.host}")
     private String host;
@@ -18,13 +24,15 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
     }
 
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
