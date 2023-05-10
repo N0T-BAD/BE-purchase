b35aa1f0f1fbfa8021282fb7af92ebb2e9d38a30
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-10 23:36:47 +0900

    feat: 결제 승인 서비스 코드작성, 추가적인 DB 저장 로직 필요

--- purchase-service/src/main/java/com/blockpage/purchaseservice/application/service/PaymentService.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/application/service/PaymentService.java
@@ -1,12 +1,19 @@
 package com.blockpage.purchaseservice.application.service;
 
+import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayApprovalParams;
+import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayApprovalResponse;
 import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayReadyParams;
 import com.blockpage.purchaseservice.adaptor.external.kakao.apispec.KakaoPayReadyResponse;
-import com.blockpage.purchaseservice.adaptor.infrastructure.redis.RedisRepository;
 import com.blockpage.purchaseservice.adaptor.web.view.KakaoPayReadyView;
-import com.blockpage.purchaseservice.application.port.in.KakaoPayInPortDto;
+import com.blockpage.purchaseservice.adaptor.web.view.KakaoPaySuccessView;
+import com.blockpage.purchaseservice.application.port.in.KakaoPayApprovalInDto;
+import com.blockpage.purchaseservice.application.port.in.KakaoPayReadyInDto;
 import com.blockpage.purchaseservice.application.port.in.KakaoPayUseCase;
+import com.blockpage.purchaseservice.application.port.out.KakaoPayApprovalPort;
 import com.blockpage.purchaseservice.application.port.out.KakaoPayReadyPort;
+import com.blockpage.purchaseservice.application.port.out.PaymentOutDto;
+import com.blockpage.purchaseservice.application.port.out.RedisForGetPort;
+import com.blockpage.purchaseservice.application.port.out.RedisForSavePort;
 import com.blockpage.purchaseservice.domain.Payment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,16 +25,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentService implements KakaoPayUseCase {
 
     private final KakaoPayReadyPort kakaoPayReadyPort;
-    private final RedisRepository repository;
+    private final KakaoPayApprovalPort kakaoPayApprovalPort;
+    private final RedisForSavePort redisForSavePort;
+    private final RedisForGetPort redisForGetPort;
 
     @Override
-    public KakaoPayReadyView kakaoPayReady(KakaoPayInPortDto kakaoPayInPortDto) {
-        Long memberId = kakaoPayInPortDto.getMemberId();
+    public KakaoPayReadyView kakaoPayReady(KakaoPayReadyInDto kakaoPayReadyInDto) {
+        Long memberId = kakaoPayReadyInDto.getMemberId();
         Payment payment = new Payment(memberId);
         payment.createOrderNumber();
+
         KakaoPayReadyParams kakaoPayReadyParams = KakaoPayReadyParams.addEssentialParams(memberId, payment.getOrderNumber(),
-            kakaoPayInPortDto);
-        KakaoPayReadyResponse kakaoPayReadyResponse = kakaoPayReadyPort.ready(kakaoPayReadyParams);
-        return new KakaoPayReadyView(kakaoPayReadyResponse.getTid(), kakaoPayReadyResponse.getNext_redirect_pc_url());
+            kakaoPayReadyInDto);
+        KakaoPayReadyResponse response = kakaoPayReadyPort.ready(kakaoPayReadyParams);
+        redisForSavePort.savePaymentReceipt(PaymentOutDto.toDto(response.getTid(), memberId, payment.getOrderNumber(), kakaoPayReadyInDto));
+        return new KakaoPayReadyView(response.getTid(), response.getNext_redirect_pc_url());
+    }
+
+    @Override
+    public KakaoPaySuccessView kakaoPayApproval(KakaoPayApprovalInDto kakaoPayApprovalINDto) {
+        PaymentOutDto paymentOutDto = redisForGetPort.getPaymentReceiptByMemberId(kakaoPayApprovalINDto.getMemberId().toString());
+        KakaoPayApprovalParams kakaoPayApprovalParams = KakaoPayApprovalParams.addEssentialParams(kakaoPayApprovalINDto, paymentOutDto);
+        KakaoPayApprovalResponse response = kakaoPayApprovalPort.approval(kakaoPayApprovalParams);
+
+        //결제 내역 저장
+
+        return new KakaoPaySuccessView(response.getPartner_user_id(),
+            response.getPartner_order_id(),
+            response.getPayment_method_type(),
+            response.getAmount(),
+            response.getItem_name(),
+            response.getQuantity(),
+            response.getCreated_at(),
+            response.getApproved_at()
+        );
     }
 }
