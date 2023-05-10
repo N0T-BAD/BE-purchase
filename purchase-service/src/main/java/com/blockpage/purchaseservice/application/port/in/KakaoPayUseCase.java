505d906cfe1eb383441576553b894e9d17b67d9a
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-10 23:33:57 +0900

    feat: 결재 승인 요청 유스케이스 추가

--- purchase-service/src/main/java/com/blockpage/purchaseservice/application/port/in/KakaoPayUseCase.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/application/port/in/KakaoPayUseCase.java
@@ -1,8 +1,11 @@
 package com.blockpage.purchaseservice.application.port.in;
 
 import com.blockpage.purchaseservice.adaptor.web.view.KakaoPayReadyView;
+import com.blockpage.purchaseservice.adaptor.web.view.KakaoPaySuccessView;
 
 public interface KakaoPayUseCase {
 
-    KakaoPayReadyView kakaoPayReady(KakaoPayInPortDto kakaoPayInPortDto);
+    KakaoPayReadyView kakaoPayReady(KakaoPayReadyInDto kakaoPayReadyInDto);
+
+    KakaoPaySuccessView kakaoPayApproval(KakaoPayApprovalInDto kakaoPayApprovalINDto);
 }
