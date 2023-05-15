29e163292a9fc3566d384270798e3c36bebb14ae
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-15 12:19:04 +0900

    feat: 구매후 반환 타입 Dto 새로 정의

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/mysql/persistence/purchasePersistenceAdaptorPersistence.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/mysql/persistence/purchasePersistenceAdaptorPersistence.java
@@ -3,11 +3,15 @@ package com.blockpage.purchaseservice.adaptor.infrastructure.mysql.persistence;
 import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.MemberHasEpisodeBMEntity;
 import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.MemberHasNftEntity;
 import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.MemberHasProfileSkinEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.NftEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.ProfileSkinEntity;
 import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.repository.MemberHasEpisodeBMRepository;
 import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.repository.MemberHasNftRepository;
 import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.repository.MemberHasProfileSkinRepository;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.repository.NftRepository;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.repository.ProfileSkinRepository;
 import com.blockpage.purchaseservice.application.port.out.PurchasePersistencePort;
-import com.blockpage.purchaseservice.application.service.PurchaseService.PurchaseDto;
+import com.blockpage.purchaseservice.domain.Purchase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,26 +22,27 @@ public class purchasePersistenceAdaptorPersistence implements PurchasePersistenc
     private final MemberHasProfileSkinRepository memberHasProfileSkinRepository;
     private final MemberHasNftRepository memberHasNftRepository;
     private final MemberHasEpisodeBMRepository memberHasEpisodeBMRepository;
+    private final ProfileSkinRepository profileSkinRepository;
+    private final NftRepository nftRepository;
 
     @Override
-    public Long saveProfileSkin(PurchaseDto purchaseOutDto) {
-
-        MemberHasProfileSkinEntity memberHasProfileSkinEntity = memberHasProfileSkinRepository.save(
-            MemberHasProfileSkinEntity.toEntity(purchaseOutDto));
-        return memberHasProfileSkinEntity.getId();
+    public MemberHasProfileSkinEntityDto saveProfileSkin(Purchase purchase) {
+        ProfileSkinEntity profileSkinEntity = profileSkinRepository.findById(purchase.getProfileSkinFk().getId()).get();
+        MemberHasProfileSkinEntity memberHasProfileSkinEntity = memberHasProfileSkinRepository.save(MemberHasProfileSkinEntity.toEntity(purchase, profileSkinEntity));
+        return MemberHasProfileSkinEntityDto.toDto(memberHasProfileSkinEntity);
     }
 
     @Override
-    public Long saveEpisodeBM(PurchaseDto purchaseOutDto) {
-        MemberHasEpisodeBMEntity memberHasEpisodeBMEntity = memberHasEpisodeBMRepository.save(
-            MemberHasEpisodeBMEntity.toEntity(purchaseOutDto));
-        return memberHasEpisodeBMEntity.getId();
+    public MemberHasEpisodeBMEntityDto saveEpisodeBM(Purchase purchase) {
+        MemberHasEpisodeBMEntity memberHasEpisodeBMEntity = memberHasEpisodeBMRepository.save(MemberHasEpisodeBMEntity.toEntity(purchase));
+        return MemberHasEpisodeBMEntityDto.toDto(memberHasEpisodeBMEntity);
 
     }
 
     @Override
-    public Long saveNft(PurchaseDto purchaseOutDto) {
-        MemberHasNftEntity memberHasNftEntity = memberHasNftRepository.save(MemberHasNftEntity.toEntity(purchaseOutDto));
-        return memberHasNftEntity.getId();
+    public MemberHasNftEntityDto saveNft(Purchase purchase) {
+        NftEntity nftEntity = nftRepository.findById(purchase.getNftFk().getId()).get();
+        MemberHasNftEntity memberHasNftEntity = memberHasNftRepository.save(MemberHasNftEntity.toEntity(purchase, nftEntity));
+        return MemberHasNftEntityDto.toDto(memberHasNftEntity);
     }
 }
--- purchase-service/src/main/java/com/blockpage/purchaseservice/application/port/out/PurchasePersistencePort.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/application/port/out/PurchasePersistencePort.java
@@ -1,12 +1,80 @@
 package com.blockpage.purchaseservice.application.port.out;
 
-import com.blockpage.purchaseservice.application.service.PurchaseService.PurchaseDto;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.MemberHasEpisodeBMEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.MemberHasNftEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.MemberHasProfileSkinEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.NftEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.entity.ProfileSkinEntity;
+import com.blockpage.purchaseservice.adaptor.infrastructure.mysql.value.PersistType;
+import com.blockpage.purchaseservice.domain.Purchase;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
 
 public interface PurchasePersistencePort {
 
-    Long saveProfileSkin(PurchaseDto purchaseOutDto);
+    MemberHasProfileSkinEntityDto saveProfileSkin(Purchase purchase);
 
-    Long saveEpisodeBM(PurchaseDto purchaseOutDto);
+    MemberHasEpisodeBMEntityDto saveEpisodeBM(Purchase purchase);
 
-    Long saveNft(PurchaseDto purchaseOutDto);
+    MemberHasNftEntityDto saveNft(Purchase purchase);
+
+    @Builder
+    @Getter
+    class MemberHasProfileSkinEntityDto {
+
+        private Long id;
+        private ProfileSkinEntity profileSkinEntity;
+        private Long memberId;
+        private Boolean defaultSkin;
+
+        public static MemberHasProfileSkinEntityDto toDto(MemberHasProfileSkinEntity entity) {
+            return MemberHasProfileSkinEntityDto.builder()
+                .id(entity.getId())
+                .memberId(entity.getMemberId())
+                .profileSkinEntity(entity.getProfileSkinEntity())
+                .defaultSkin(entity.getDefaultSkin())
+                .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    class MemberHasEpisodeBMEntityDto {
+
+        private Long id;
+        private Long memberId;
+        private Long episodeId;
+        private Long webtoonId;
+        private PersistType persistType;
+        private LocalDateTime expiredDate;
+
+        public static MemberHasEpisodeBMEntityDto toDto(MemberHasEpisodeBMEntity entity) {
+            return MemberHasEpisodeBMEntityDto.builder()
+                .id(entity.getId())
+                .memberId(entity.getMemberId())
+                .episodeId(entity.getEpisodeId())
+                .webtoonId(entity.getWebtoonId())
+                .persistType(entity.getPersistType())
+                .expiredDate(entity.getExpiredDate())
+                .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    class MemberHasNftEntityDto {
+
+        private Long id;
+        private NftEntity nftEntity;
+        private Long memberId;
+
+        public static MemberHasNftEntityDto toDto(MemberHasNftEntity entity) {
+            return MemberHasNftEntityDto.builder()
+                .id(entity.getId())
+                .memberId(entity.getMemberId())
+                .nftEntity(entity.getNftEntity())
+                .build();
+        }
+    }
 }
