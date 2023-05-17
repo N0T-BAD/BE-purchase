b9c9898210fefb58c6a61eb4adb94896bff9714d
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-17 13:35:57 +0900

    feat: OpenFeign 예외처리 및 서킷브레이크 설정

--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/external/block/configuration/BlockServiceOpenFeignConfig.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/external/block/configuration/BlockServiceOpenFeignConfig.java
@@ -1,6 +1,9 @@
 package com.blockpage.purchaseservice.adaptor.infrastructure.external.block.configuration;
 
+import feign.Logger.Level;
 import feign.RequestInterceptor;
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,21 @@ public class BlockServiceOpenFeignConfig {
 
     @Value("${block.service.api.content-type}")
     private String blockServiceContentType;
+
+    @Bean
+    public feign.Logger.Level feignLoggerLevel() {
+        return Level.FULL;
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new GlobalErrorDecoder();
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(); //this(100, *SECONDS*.toMillis(1), 5);
+    }
 
     @Bean
     public RequestInterceptor requestInterceptor() {
--- purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/external/block/controller/BlockServiceOpenFeign.java
+++ purchase-service/src/main/java/com/blockpage/purchaseservice/adaptor/infrastructure/external/block/controller/BlockServiceOpenFeign.java
@@ -4,15 +4,28 @@ package com.blockpage.purchaseservice.adaptor.infrastructure.external.block.cont
 import com.blockpage.purchaseservice.adaptor.infrastructure.external.block.configuration.BlockServiceOpenFeignConfig;
 import com.blockpage.purchaseservice.adaptor.infrastructure.external.block.requestbody.BlockPayRequestParams;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-@FeignClient(name = "block-service", url = "${block.service.api.url}", configuration = BlockServiceOpenFeignConfig.class)
+@FeignClient(name = "block-service", url = "${block.service.api.url}",
+    configuration = BlockServiceOpenFeignConfig.class,
+    fallback = BlockServiceOpenFeign.FallBack.class)
 public interface BlockServiceOpenFeign {
 
     @PutMapping(value = "/v1/blocks")
     ResponseEntity blockPay(@RequestBody BlockPayRequestParams blockPayRequestParams);
+
+    @Component
+    class FallBack implements BlockServiceOpenFeign {
+
+        @Override
+        public ResponseEntity blockPay(BlockPayRequestParams blockPayRequestParams) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+        }
+    }
+
 }
 
