70f1de795f5ce7a1c8c87861ac871958806ccc17
geunskoo <oryukdo3@pusan.ac.kr>
2023-05-24 11:55:46 +0900

    feat: @EnableEurekaClients추가

--- src/main/java/com/blockpage/purchaseservice/PurchaseServiceApplication.java
+++ src/main/java/com/blockpage/purchaseservice/PurchaseServiceApplication.java
@@ -2,11 +2,13 @@ package com.blockpage.purchaseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableEurekaClient
 @SpringBootApplication
 public class PurchaseServiceApplication {
 
